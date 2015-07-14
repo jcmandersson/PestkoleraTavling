e461c885b888fc94df8f10efe285402efdd66eca
Jonas Andersson <jcm.andersson@gmail.com>
2015-07-14 12:00:23 +0200

    Removed beta prefix on url

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var vote_function = function () {
   var tid = $(this).parent().attr('data-id');
   var $this = $(this);
   var vote = ($this.hasClass('upvote') ? 1 : -1);
-  $.get('http://beta.pestkolera.se/app_top.php?vote=' + vote + '&tid=' + tid, function (data) {
+  $.get('http://pestkolera.se/app_top.php?vote=' + vote + '&tid=' + tid, function (data) {
     $this.parent().find('.active').removeClass('active');
     $this.addClass('active');
   });
@@ -26,14 +26,14 @@ var get_questions_factory = function (option) {
 };
 
 $(document).ready(function () {
-  $.get('http://beta.pestkolera.se/app_top.php', get_questions_factory('top'));
-  $.get('http://beta.pestkolera.se/app_top.php?latest', get_questions_factory('latest'));
+  $.get('http://pestkolera.se/app_top.php', get_questions_factory('top'));
+  $.get('http://pestkolera.se/app_top.php?latest', get_questions_factory('latest'));
 
   $('body')
     .on('click', '.upvote,.downvote', vote_function)
     .on('click', '.show_all', function () {
       $('.latestQuestions tr:not(.hide,.head)').remove();
       $(this).remove();
-      $.get('http://beta.pestkolera.se/app_top.php?all', get_questions_factory('latest'));
+      $.get('http://pestkolera.se/app_top.php?all', get_questions_factory('latest'));
     });
 });
